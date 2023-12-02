a22c349ca23db06ed62c5d21526775aaaddf696b
MinitrueSecretary <wipugeng@hotmail.com>
2023-12-03 00:15:12 +0700

    connect registration to backend

--- src/components/homePage/registerPage.tsx
+++ src/components/homePage/registerPage.tsx
@@ -3,14 +3,30 @@ import { TextField, Button ,Box, Typography, FormControl, FormControlLabel, Form
 import {useRouter} from 'next/router';
 const RegisterPage: React.FC = () => {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [walletID, setWalletID] = useState('');
     const [role, setRole] = useState('');
+    const [regFail, setRegFail] = useState(false);
     const router = useRouter();
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault(); 
-        // onSubmit({ username, password, ethereumAccount: walletID });
-        console.log(username,password,walletID,role);
+        console.log(JSON.stringify({ name: username, email: email, password: password, role: role }));
+        const res = await fetch('http://localhost:5400/api/v1/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name: username, email: email, password: password, role: role }),
+        });
+        const data = await res.json();
+        if(data.success){
+            console.log('register success');
+            router.push('/');
+        }
+        else{
+            setRegFail(true);
+            console.log('register failed');
+        }
     };
 
     return (
@@ -18,6 +34,7 @@ const RegisterPage: React.FC = () => {
             <Typography variant="h1" gutterBottom align='center'>BlockGacha</Typography>
             <Typography variant="h3" gutterBottom align='center'>Register</Typography>
             <form onSubmit={handleSubmit} style={{textAlign:'left',marginLeft:"10%",marginRight:"10%"}}>
+
                 <TextField
                     label="Username"
                     value={username}
@@ -25,6 +42,13 @@ const RegisterPage: React.FC = () => {
                     margin="normal"
                     style={{width: '100%'}}
                 />
+                <TextField
+                    label="Email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    margin="normal"
+                    style={{width: '100%'}}
+                />                
                 <TextField
                     label="Password"
                     type="password"
@@ -34,13 +58,7 @@ const RegisterPage: React.FC = () => {
                     style={{width: '100%'}}
                     margin="normal"
                 />
-                <TextField
-                    label="Wallet ID"
-                    value={walletID}
-                    onChange={(event) => setWalletID(event.target.value)}
-                    margin="normal"
-                    style={{width: '100%'}}
-                />
+
                 <br/>
                 <FormControl style={{alignContent:"left",marginBottom:"40px"}}>
                 <FormLabel id="role-select" >Role</FormLabel>
@@ -55,9 +73,13 @@ const RegisterPage: React.FC = () => {
                 </RadioGroup>
                 </FormControl>
 
-                <Box textAlign='center'><Button type="submit" variant="contained" color="primary" style={{width:"30%",fontSize: '1.5rem',}}>
+                <Box textAlign='center'>
+                    {regFail && <Typography variant="body1" style={{ color: 'red' }}>Registration failed</Typography>}
+
+                    <Button type="submit" variant="contained" color="primary" style={{width:"30%",fontSize: '1.5rem',}}>
                     Register
-                </Button></Box>
+                    </Button>
+                </Box>
             </form>
         </Box>
     );
